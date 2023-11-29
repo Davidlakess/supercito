df79384c61cb31628c7b9f4c8982de043fabfef4
lakess005 <lakess00511@gmail.com>
2023-11-28 20:26:33 -0600

    correcion carritos detalle

--- app/Models/DetalleCarritos.php
+++ app/Models/DetalleCarritos.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class DetalleCarritos extends Model
 {
     use HasFactory;
-    protected $table = "DetalleCarritos";
+    protected $table = "detallecarritos";
 }
